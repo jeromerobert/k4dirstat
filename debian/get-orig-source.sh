69f949a1ffbf61df0a0e564cd11f528c4884d096
jeromerobert <jeromerobert@users.sf.net>
2011-11-11 21:22:18 +0100

    Change upstream owner and URL

--- debian/get-orig-source.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh -x 
-git_tree_ish=$1
-debian_dir=$(dirname $0)
-git_dir=$(mktemp -td k4dirstat.XXX)
-version=$(dpkg-parsechangelog -l$debian_dir/changelog | sed -ne 's/^Version: \(\([0-9]\+\):\)\?\(.*\)-.*/\3/p')
-#remote git archive does not work with this repository so we must clone first
-git clone --bare http://grumpypenguin.org/~josh/kdirstat.git $git_dir
-git --git-dir=$git_dir archive --prefix=k4dirstat-$version/ $git_tree_ish | gzip > k4dirstat_$version.orig.tar.gz
-#Keep the tmp bare repository for conveniance
