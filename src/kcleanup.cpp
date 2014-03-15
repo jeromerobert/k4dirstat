1ad2e96d73fa06cd9be0f3749b337c03575016aa
Jerome Robert <jeromerobert@users.sf.net>
2014-03-15 16:54:34 +0100

    Properly escape %p and %n in cleaning actions

--- src/kcleanup.cpp
+++ src/kcleanup.cpp
@@ -318,11 +318,10 @@ KCleanup::expandVariables( const KFileInfo *	item,
 			   const QString &	unexpanded ) const
 {
     QString expanded = unexpanded;
-
-    expanded.replace( QRegExp( "%p" ),
-		      "'" + QString::fromLocal8Bit( item->url() )  + "'" );
-    expanded.replace( QRegExp( "%n" ),
-		      "'" + QString::fromLocal8Bit( item->name() ) + "'" );
+    QString url = QString::fromLocal8Bit( item->url() ).replace("'", "'\\''");
+    expanded.replace( QRegExp( "%p" ), "'" + url + "'" );
+    QString name = QString::fromLocal8Bit( item->name() ).replace("'", "'\\''");
+    expanded.replace( QRegExp( "%n" ), "'" + name + "'" );
 
    // if ( KDE::versionMajor() >= 3 && KDE::versionMinor() >= 4 )
 	expanded.replace( QRegExp( "%t" ), "trash:/" );
